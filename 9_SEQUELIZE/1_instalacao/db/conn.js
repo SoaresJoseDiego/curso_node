016fe1049e4c3025e6d82317a2605010a3391cd9
Diego Soares <dsadamovich@gmail.com>
2025-03-18 16:42:50 -0300

    Aula

---
+++ 9_SEQUELIZE/1_instalacao/db/conn.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize('nodesequelize', 'root', '',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+
+try{
+    sequelize.authenticate();
+    console.log('Conectado ao banco de dados com sucesso');
+}catch(err){   
+    console.log('Não foi possivel conectar ao banco de dados', err);
+    
+}
+
+
+module.exports = sequelize;
