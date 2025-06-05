845af85f5cb5173dc4e9e9192a54f64ea73e0063
Diego Soares <dsadamovich@gmail.com>
2025-06-05 17:51:50 -0300

    DSADSA

---
+++ 12_MONGODB/6_excluindo_dados/controllers/ProductsController.js
@@ -0,0 +1,43 @@
+const Product = require("../models/Products");
+const { ObjectId } = require("mongodb");
+
+module.exports = class ProductController {
+  static async showProducts(req, res) {
+    const products = await Product.getProducts();
+    res.render("products/all", { products });
+  }
+
+  static createProduct(req, res) {
+    res.render("products/create");
+  }
+
+  static async createProductPost(req, res) { // Adicionado async
+    const name = req.body.name;
+    const image = req.body.image;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    const product = new Product(name, image, price, description);
+    await product.save(); // Adicionado await
+
+    res.redirect("/products");
+  }
+
+  static async getProduct(req, res) {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).send("ID inválido");
+    }
+    const product = await Product.getProductById(id);
+    res.render('products/product', { product });
+  }
+
+  static async removeProduct(req, res) {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).send("ID inválido");
+    }
+    await Product.removeProductById(id);
+    res.redirect('/products');
+  }
+};
